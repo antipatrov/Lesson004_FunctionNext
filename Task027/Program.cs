170e4a65b796668e8acb6302998b8352e3f457fe
Yuri_Antipatrov <89209578005@yandex.ru>
2022-08-21 23:47:58 +0300

    Task027

---
+++ Task027/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+
+
+Console.WriteLine("Input number:  ");//Вводим число
+
+int array = Convert.ToInt32(Console.ReadLine());
+string str = array.ToString();
+//Console.WriteLine(str);
+
+int[] arrayB = new int[str.Length];
+int sumElmMass = 0;
+for (int i = 0; i < str.Length; i++)
+{
+    
+    arrayB[i] = Convert.ToInt32(str[i].ToString());//int.Parse(str[i].ToString());
+    sumElmMass = sumElmMass + arrayB[i];
+    
+
+
+}
+Console.WriteLine("sumElmMass = " + sumElmMass);
+
+
+
+
